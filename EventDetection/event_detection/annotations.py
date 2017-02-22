9dd86bd5ace3e060c5cf5a301869ee9f25a6c74a
Tomas Kala <kalatoma@fel.cvut.cz>
2017-02-22 23:32:00 +0100

    Added docstrings

--- EventDetection/event_detection/annotations.py
+++ EventDetection/event_detection/annotations.py
@@ -96,18 +96,35 @@ def load_documents(docids, fetcher):
     return dict(zip(docids, documents))
 
 
-# TODO: Read something about the parameters `a` (alpha), `l` (lambda) and `r` -- changing the value from l=6.0, r=1.0
-# TODO: to l=4.0, r=0.3 (as used in the original paper) improved the summaries quite a bit.
-
 class Summarizer:
-    def __init__(self, w2v_model, min_sent_len=5, a=5.0, beta_=0.5, lambda_=4.0, r_=0.3):
+    """
+    Based on 'Lin, Bilmes, 2010, Multi-document Summarization via Budgeted Maximization of Submodular Functions'
+    and 'Mogren, Kågebäck, Dubhashi, 2015, Extractive Summarization by Aggregating Multiple Similarities'.
+    """
+
+    def __init__(self, w2v_model, min_sent_len=5, avg_cluster_size=10, a=5.0, beta_=0.5, lambda_=4.0, r_=0.3):
+        """
+        Initialize the summarizer without actually summarizing anything - use the `summarize` method for that. The
+        default values are set to those performing the best in the paper.
+        :param w2v_model: trained Word2Vec model
+        :param min_sent_len: minimum length of a sentence to be considered for summarization
+        :param avg_cluster_size: average number of sentences to put into each cluster
+        :param a: >= 0, defines `alpha` = `a` / `n_sentences`; `alpha` defines the threshold between inter-similarity
+            of a summary and outer-similarity of a summary to the whole sentence set
+        :param beta_: <- [0,1]; `beta` is the coefficient of the convex combination of cluster similarities and query
+            similarity, set `beta` = 1 to dismiss the query effect in diversity computation entirely
+        :param lambda_: >= 0; `lambda` controls the influence of sentence diversity, the higher `lambda`, the lower
+            the summary diversity
+        :param r_: >= 0; `r` scales the constraint values of individual sentences, the higher `r`, the higher role
+            the sentence constraints play as opposed to the objective function gain
+        """
         self.w2v_model = w2v_model
         self.min_sent_len = min_sent_len
 
         assert a >= 0, 'The parameter `a` must be >= 0'
         assert 0 <= beta_ <= 1, 'The parameter `beta` must be in [0,1]'
         assert lambda_ >= 0, 'The parameter `lambda` must be >= 0'
-        assert r_ > 0, 'The parameter `r` must be > 0'
+        assert r_ >= 0, 'The parameter `r` must be >= 0'
 
         self.a = a
         self.alpha_ = None  # `alpha` = `a` / `n_sentences`
@@ -117,13 +134,22 @@ class Summarizer:
 
         self.n_sentences = None
         self.n_clusters = None  # `n_clusters` = `n_sentences` // `avg_cluster_size`
-        self.avg_cluster_size = 10
+        self.avg_cluster_size = avg_cluster_size
 
         self.similarity_matrix = None  # Aggregates pairwise similarities between sentences.
-        self.kw_similarities = None  # Vector sentence-event_keywords similarities.
-        self.cluster_similarity = None  # Pairwise similarities between sentences and clusters.
+        self.kw_similarities = None  # Vector of sentence-event_keywords similarities.
+        self.sentence_cluster_similarities = None  # Pairwise similarities between sentences and clusters.
 
     def summarize(self, event_keywords, documents, budget, constraint_type):
+        """
+        Summarize the given event with a given document set under the given constraint type and budget.
+        :param event_keywords: keyword representation of the event
+        :param documents: document representation of the event, must be instances of the `LemmatizedDocument`
+            class defined above
+        :param budget: maximum value the constraints can attain altogether
+        :param constraint_type: whether to limit the number of words ('words') or the number of sentences ('sentences')
+        :return: list of sentences selected for summarization in the same order as they appeared in the given documents
+        """
         t = time()
 
         sentences_forms, sentences_lemma, sentences_pos = self._docs2sents(documents)
@@ -153,6 +179,16 @@ class Summarizer:
         return [sentences_forms[index] for index in selected_indices]
 
     def _docs2sents(self, documents):
+        """
+        Retrieve valid sentences to be considered for summarization. A sentence will be considered if it is at least
+        `self.min_sent_len` words long (including punctuation) and contains at least 1 Noun and ` Verb. Additionally,
+        retrieve only unique sentences according to their lemmatized representation.
+        :param documents: the documents to be converted, must be instances of the `LemmatizedDocument`
+            class defined above
+        :return: triple (sentences with word forms, sentences with word lemmas, word parts of speech tags) in
+            corresponding order
+        """
+
         def is_valid_sentence(sentence_form, sentence_pos):
             has_noun = any(sent_pos[0] == 'N' for sent_pos in sentence_pos)
             has_verb = any(sent_pos[0] == 'V' for sent_pos in sentence_pos)
@@ -180,7 +216,13 @@ class Summarizer:
         return sentences_forms, sentences_lemma, sentences_pos
 
     def _cluster_similarities(self):
-        distance_matrix = 1.0 - self.similarity_matrix  # TODO: 1 / (similarity + 1)?
+        """
+        Cluster the sentences into `self.n_clusters` clusters after converting the similarity matrix to a
+        distance matrix.
+        The clusters will be represented by a matrix `self.sentence_cluster_similarities` mapping sentences to
+        their similarities to each cluster. This is useful to quickly compute the current summary diversity.
+        """
+        distance_matrix = 1.0 - self.similarity_matrix
 
         # To avoid floating point precision errors.
         np.clip(distance_matrix, 0, 1, out=distance_matrix)
@@ -193,9 +235,15 @@ class Summarizer:
         cluster_adjacency = np.zeros(shape=(self.n_sentences, self.n_clusters), dtype=int)
         cluster_adjacency[sentence_ids, labels] = 1
 
-        self.cluster_similarity = self.similarity_matrix @ cluster_adjacency
+        self.sentence_cluster_similarities = self.similarity_matrix @ cluster_adjacency
 
     def _precompute_similarities(self, event_keywords, sentences_lemma, sentences_pos):
+        """
+        Precompute the matrix aggregating the various sentence similarities. The elements are normalized to [0,1].
+        :param event_keywords: keyword representation of the summarized event
+        :param sentences_lemma: lemmatized sentences to extract the summary from
+        :param sentences_pos: parts of speech tags of the sentences in the same order
+        """
         sentence_vectors = self._sents2vecs(sentences_lemma)
         self.similarity_matrix = cosine_similarity(sentence_vectors, dense_output=True)
 
@@ -228,10 +276,21 @@ class Summarizer:
 
     @staticmethod
     def _sents2vecs(sentences):
+        """
+        Transform the sentences into TFIDF space.
+        :param sentences: lemmatized sentences
+        :return: TFIDF matrix
+        """
         tfidf = TfidfVectorizer(sublinear_tf=True, analyzer=lambda doc: doc, preprocessor=lambda doc: doc)
         return tfidf.fit_transform(sentences)
 
     def _w2v_similarity(self, sentences):
+        """
+        Compute Word2Vec similarity of the sentences, defined as cosine similarity between sentence vectors
+        obtained by summing together all Word2Vec vectors of the sentence words.
+        :param sentences: lemmatized sentences
+        :return: matrix of cosine similarities between the vectors
+        """
         w2v_model = self.w2v_model
         sentence_vectors = np.empty(shape=(self.n_sentences, w2v_model.vector_size), dtype=float)
 
@@ -245,11 +304,24 @@ class Summarizer:
 
     @staticmethod
     def _lsi_similarity(sentence_vectors, k):
+        """
+        Apply Latent Semantic Indexing to the sentence vectors and compute the resulting vectors similarity.
+        :param sentence_vectors: vector representation of the sentences, preferably TFIDF-scaled
+        :param k: latent space dimensionality
+        :return: matrix cosine similarities between the LSI-transformed vectors
+        """
         lsi = TruncatedSVD(n_components=k, random_state=1)
         lsi_matrix = lsi.fit_transform(sentence_vectors)
         return cosine_similarity(lsi_matrix, dense_output=True)
 
     def _tr_similarity(self, sentences, sentences_pos):
+        """
+        Compute TextRank similarity of the sentences, defined as Sim(i,j) = |i `intersection` j| / (log|i| + log|j|).
+        Only consider Nouns and Verbs for the similarity.
+        :param sentences: lemmatized sentences
+        :param sentences_pos: parts of speech tags of the sentences in the same order
+        :return: matrix of TextRank similarities
+        """
         vectorizer = CountVectorizer(binary=True, analyzer=lambda doc: doc, preprocessor=lambda doc: doc, dtype=float)
 
         pos_sentences = [[word for word, pos in zip(sentence, sentence_pos) if pos[0] in 'NV'] for
@@ -257,11 +329,18 @@ class Summarizer:
 
         bow_matrix = vectorizer.fit_transform(pos_sentences)
         log_lengths = np.log(np.fromiter(map(len, sentences), dtype=float, count=self.n_sentences))
-
         sim = (bow_matrix @ bow_matrix.T) / np.add.outer(log_lengths, log_lengths)
+
         return sim
 
     def _kw_similarity(self, sentences, keywords):
+        """
+        Compute Keyword similarity of the sentences, defined as
+        Sim(i,j) = sum_{w <- i `cap` j `cap KW}{TFIDF_w} / (|i| + |j|).
+        :param sentences: lemmatized sentences
+        :param keywords: keyword representation of the summarized event
+        :return: matrix of Keyword similarities
+        """
         tfidf = TfidfVectorizer(sublinear_tf=True, analyzer=lambda doc: doc, preprocessor=lambda doc: doc)
         tfidf_matrix = tfidf.fit_transform(sentences)
         kw_indices = [tfidf.vocabulary_[kw] for kw in keywords if kw in tfidf.vocabulary_]
@@ -271,12 +350,17 @@ class Summarizer:
 
         kw_slice_copy = kw_slice.copy()
         kw_slice_copy.data.fill(1)
-
         lengths = np.fromiter(map(len, sentences), dtype=float, count=self.n_sentences)
 
         return (kw_slice @ kw_slice_copy.T) / np.add.outer(lengths, lengths)
 
     def _greedy_summarization(self, constraints, budget):
+        """
+        The greedy optimization loop performing the summarization.
+        :param constraints: vector of sentence constraints
+        :param budget: maximum value the constraints can attain altogether
+        :return: indices of the sentences selected for summarization
+        """
         summary = []
         remainder = set(range(self.n_sentences))
 
@@ -285,30 +369,44 @@ class Summarizer:
         t = time()
 
         while len(remainder) > 0:
-            k, val = self._argmax(summary, remainder, objective_function, constraints, cost_so_far, budget)
+            k, val = self._feasible_argmax(summary, remainder, objective_function, constraints, cost_so_far, budget)
 
-            if k is not None and cost_so_far + constraints[k] <= budget and val - objective_function >= 0:
+            if k is not None and val - objective_function >= 0:
                 cost_so_far += constraints[k]
                 objective_function = val
                 summary.append(k)
             else:
+                # Can break, since the argmax selected was also feasible. Otherwise, there could be a sentence with
+                # lower objective function gain satisfying the constraints.
                 break
 
             remainder.remove(k)
 
         singleton_candidates = set(filter(lambda singleton: constraints[singleton] <= budget, range(self.n_sentences)))
         fake_constraints = np.ones(self.n_sentences, dtype=int)
-        singleton_id, singleton_val = self._argmax([], singleton_candidates, 0.0, fake_constraints, 0, budget)
+        singleton_id, singleton_val = self._feasible_argmax([], singleton_candidates, 0.0, fake_constraints, 0, budget)
 
         if singleton_val > objective_function:
             logging.info('Summarized event in %fs while filling %d out of %d budget.', time() - t,
                          constraints[singleton_id], budget)
             return [singleton_id]
-        else:
-            logging.info('Summarized event in %fs while filling %d out of %d budget.', time() - t, cost_so_far, budget)
-            return summary
 
-    def _argmax(self, summary, remainder, objective_function_value, constraints, cost_so_far, budget):
+        logging.info('Summarized event in %fs while filling %d out of %d budget.', time() - t, cost_so_far, budget)
+        return summary
+
+    def _feasible_argmax(self, summary, remainder, value_so_far, constraints, cost_so_far, budget):
+        """
+        Select the sentence index from `remainder` maximizing the objective function gain such that its constraint
+        added to `cost_so_far` remains feasible.
+        :param summary: indices of sentences selected for the summary so far
+        :param remainder: indices of sentences not yet selected for the summarization
+        :param value_so_far: objective function value obtained so far
+        :param constraints: vector of sentence constraints
+        :param cost_so_far: total value of constraints of the sentences selected so far
+        :param budget: maximum value the constraints can attain altogether
+        :return: tuple (index, objective function value) of the best sentence or (None, 0) if no feasible
+            sentence is found
+        """
         argmax_id = None
         max_val = -math.inf
         r = self.r_
@@ -317,7 +415,7 @@ class Summarizer:
             new_objective_value = self._quality(summary + [sentence_id])
             scaled_constraint = constraints[sentence_id] ** r
 
-            function_gain = (new_objective_value - objective_function_value) / scaled_constraint
+            function_gain = (new_objective_value - value_so_far) / scaled_constraint
 
             if cost_so_far + constraints[sentence_id] <= budget and function_gain > max_val:
                 argmax_id = sentence_id
@@ -325,20 +423,36 @@ class Summarizer:
 
         if argmax_id is None:
             return None, 0
-        else:
-            return argmax_id, self._quality(summary + [argmax_id])
+
+        return argmax_id, self._quality(summary + [argmax_id])
 
     def _quality(self, summary):
+        """
+        The objective function being optimized. Measures the similarity of the summary to the whole set of sentences
+        and its diversity.
+        :param summary: indices of sentences selected for the summary so far
+        :return: objective function value
+        """
         return self._similarity(summary) + self.lambda_ * self._diversity(summary)
 
     def _similarity(self, summary):
+        """
+        Similarity of the summary to the whole set of sentences.
+        :param summary: indices of sentences selected for the summary so far
+        :return: similarity
+        """
         inter_similarity = np.sum(self.similarity_matrix[:, summary], axis=1)
         outer_similarity = np.sum(self.similarity_matrix, axis=1)
 
         return np.sum(np.minimum(inter_similarity, self.alpha_ * outer_similarity))
 
     def _diversity(self, summary):
-        summary_reward = np.mean(self.cluster_similarity[summary], axis=0)
+        """
+        Diversity of the summary.
+        :param summary: indices of sentences selected for the summary so far
+        :return: diversity
+        """
+        summary_reward = np.mean(self.sentence_cluster_similarities[summary], axis=0)
         keyword_reward = self.kw_similarities[summary]
 
         return np.sum(np.sqrt(self.beta_ * summary_reward + (1 - self.beta_) * keyword_reward))
